c1615336a0caa23d1bb0bf0276d27e4f65e1efda
wanglufei <2686221966@qq.com>
2022-04-10 14:13:14 +0800

    add axios

---
+++ src/utils/axios.js
@@ -0,0 +1,74 @@
+//axios 是一个基于promise的HTTP库，可以在浏览器和node.js中使用
+//从浏览器中创建XMLHttpRequests
+//从node.js中创建Http请求、支持Promise API、拦截请求和响应、转换请求数据和响应数据、取消请求、自动转换JSON数据、客户端支持防御XSRF
+
+import axios from 'axios'
+import {Message} from 'element-ui';
+
+//添加响应拦截器
+axios.interceptors.response.use(success => {
+    //业务逻辑错误
+    if (success.status && success.status == 200) {
+        if (success.data.code == 500 || success.data.code == 401 || success.data.code == 403) {
+            Message.error({message: success.data.message});
+            return;
+        }
+        if (success.data.message) {
+            Message.success({message: success.data.message})
+        }
+    }
+    return success.data;
+}, error => {
+    if (error.response.code == 504 || error.response.code = 4004) {
+        Message.error({message: "服务器打小差了。。。"})
+    } else if (error.response.code = 403) {
+        Message.error({message: '权限不足，请联系管理员！'});
+    } else if (error.response.code == 401) {
+        Message.error({message: '尚未登录，请登录'});
+        //跳转到登陆页
+        router.replace('/');
+        //如果所有的状态码都不在这
+    } else {
+        if (error.response.data.message) {
+            Message.error({message: error.response.data.message});
+        } else {
+            Message.error({message: '未知错误！'});
+        }
+    }
+    return;
+});
+
+//前置的路径
+let base = '';
+//传送json格式的post请求
+export const postRequest = (url, params) => {
+    return axios({
+        method: 'post',
+        url: `${base}${url}`,
+        data: params
+    })
+}
+//传递json的put请求
+export const putRequest = (url, params) => {
+    return axios({
+        method: 'put',
+        url: `${base}${url}`,
+        data: params
+    })
+}
+//传递json的get请求
+export const getRequest = (url, params) => {
+    return axios({
+        method: 'get',
+        url: `${base}${url}`,
+        data: params
+    })
+}
+//传递json的delete请求
+export const deleteRequest = (url, params) => {
+    return axios({
+        method: 'delete',
+        url: `${base}${url}`,
+        data: params
+    })
+}
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,21 +1,27 @@
 <template>
   <div>
-    <!--表单数据的对象-->
-    <el-form ref="LoginFrom" :model="login_form_data" class="login_container">
-      <h3>系统登陆</h3>
-      <el-form-item label="用户名" size="large">
-        <el-input type="text" auto-complete="false" v-model="login_form_data.username" placeholder="请输入用户名"></el-input>
+    <!--v-model是vue.js中内置的双向数据绑定-->
+    <!--v-bind:model="" 相当于 :model="" 这种只是将父组件的数据传递到了子组件，并没有实现子组件和父组件数据的双向绑定。-->
+    <!--:model表单数据的对象-->
+    <!--rules表单验证规则 当然也可以自定义:model="login_rule_from"-->
+    <el-form v-bind:rules="rules" ref="LoginFrom" v-bind:model="LoginFrom" id="login_container">
+      <h3 id="login_title">云E办管理系统登陆</h3>
+      <!--prop 才能和校验规则对应上-->
+      <el-form-item prop="username">
+        <el-input type="text" auto-complete="false" v-model="LoginFrom.username" placeholder="请输入用户名"></el-input>
       </el-form-item>
-      <el-form-item label="密码">
-        <el-input type="password" auto-complete="false" v-model="login_form_data.password"
+
+      <el-form-item prop="password">
+        <el-input type="password" auto-complete="false" v-model="LoginFrom.password"
                   placeholder="请输入密码"></el-input>
       </el-form-item>
-      <el-form-item label="验证码" size="small">
-        <el-input type="text" v-model="login_form_data.code" placeholder="点击按钮获取验证码"></el-input>
-      <img :src="captchaUrl">
-      <!-- <el-button @click="getCapter">获取验证码</el-button> -->
+
+      <el-form-item prop="code">
+        <el-input type="text" v-model="LoginFrom.code" placeholder="点击按钮获取验证码"
+                  style="width: 250px;margin-right: 5px"></el-input>
+        <img v-bind:src="captchaUrl" @click="updateCap">
       </el-form-item>
-      <el-checkbox v-model="checked">记住我</el-checkbox>
+      <el-checkbox v-model="checked" id="login_rem">记住我</el-checkbox>
       <el-button type="primary" @click="doLogin" style="width:100%">登陆</el-button>
     </el-form>
   </div>
@@ -27,28 +33,67 @@ export default {
   name: "Login",
   data() {
     return {
-      captchaUrl:'',
-      login_form_data: {
+      captchaUrl: '/captcha?time=' + new Date(),
+      LoginFrom: {
         username: 'admin',
         password: '123',
         code: ''
-      }
+      },
+      checked: true,
+      //使用默认的校验规则
+      rules: {
+        //trigger:'blur' 失去焦点的时候触发
+        username: [{required: true, message: '请输入用户名！', trigger: 'blur'},
+          {min: 3, max: 15, message: "长度在3-15之间", trigger: 'blur'}
+        ],
+        password: [{required: true, message: '请输入密码！', trigger: 'blur'}],
+        code: [{required: true, message: '请输入验证码！', trigger: 'blur'}]
+      },
     }
   },
   methods: {
     doLogin() {
-      alert("ok！")
+      //1.校验数据的合法性
+      // this.$refs["LoginFrom"].validate((valid)=>{
+      this.$refs.LoginFrom.validate((valid) => {
+        if (valid) {
+          alert("ok")
+        } else {
+          this.$message.error("请输入所有的字段！")
+          return false;
+        }
+      })
+      //2.跳转页面
     },
-    getCapter() {
-      alert("123")
+    updateCap() {
+      this.captchaUrl = '/captcha?time=' + new Date();
     }
-  }
+
+  },
+
 }
 </script>
 
 <style>
-.login_containergin{
+#login_container {
+  border-radius: 15px;
+  background-clip: padding-box;
+  margin: 180px auto;
+  width: 350px;
+  padding: 15px 35px 15px 35px;
+  background: white;
+  border: 1px solid #eaeaea;
+  box-shadow: 0 0 25px #cac6c6;
+}
 
+#login_title {
+  margin: 0px auto 40px auto;
+  text-align: center;
+}
+
+#login_rem {
+  text-align: left;
+  margin: 0px 0px 15px 0px;
 }
 
 </style>
